03bf6380d00e5f610b670cd1d403e6f8ef93ca2f
rkt0589 <rkthakur0589@gmail.com>
2018-03-26 11:32:15 +0530

    changed qt-can.sh for framebuffer

--- qt-can.sh
+++ qt-can.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 ts_calibrate
 while : ; do
-#export QT_QPA_EGLFS_HIDECURSOR=1
-#export QT_QPA_EGLFS_FB=/dev/fb0
+export QT_QPA_EGLFS_HIDECURSOR=1
+export QT_QPA_EGLFS_FB=/dev/fb0
 #screen1 -platform eglfs
 demo_can_adc_gpio_qt
 done
